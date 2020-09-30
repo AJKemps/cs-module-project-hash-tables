236bce9cfd94ed5964c7c1d2fb8fd8346777fd27
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-30 19:27:39 -0400

    done with wed mvp

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -2,12 +2,25 @@ import random
 
 # Read in all the words in one go
 with open("input.txt") as f:
-    words = f.read()
+    words = f.read().split()
+
 
 # TODO: analyze which words can follow other words
 # Your code here
 
+print(words)
+
+markov = {}
+
+for i in range(len(words)-1):
+    if words[i] not in markov:
+        markov[words[i]] = {words[i+1]}
+    if words[i] in markov:
+        markov[words[i]] = {*markov[words[i]], words[i+1]}
+
+for k, v in sorted(markov.items(), key=lambda item: item[0]):
+    print(f'{k}: {v}')
+
 
 # TODO: construct 5 random sentences
 # Your code here
-
