1c724083f7513509cb8ce3b92b4417d32d7bd2db
Alex Kemper <alex.j.kemper@gmail.com>
2020-09-30 16:07:16 -0400

    word count done

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,44 @@
+ignore = ['"', ":", ";", ",", ".", "-", "+", "=", "/", "\\",
+          "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"]
+
+
+def remove_char(s):
+
+    word = s
+
+    for item in ignore:
+        word = word.replace(item, "")
+
+    return word
+
+
 def word_count(s):
     # Your code here
 
+    words = s.lower().split()
+    fixed_words = []
+    words_count = {}
+
+    for word in words:
+        fixed_word = remove_char(word)
+        fixed_words.append(fixed_word)
+
+    for word in fixed_words:
+        if word not in words_count:
+            words_count[word] = 0
+        count = fixed_words.count(word)
+        words_count[word] = count
+
+    if len(fixed_words) > 0:
+        if fixed_words[0] == "":
+            return {}
+    print(words_count)
+    return words_count
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
