01a869bc4083952f439cdd134456439e9b0817b5
Alex Kemper <alex.j.kemper@gmail.com>
2020-10-01 14:19:59 -0400

    markov done

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -24,3 +24,42 @@ for k, v in sorted(markov.items(), key=lambda item: item[0]):
 
 # TODO: construct 5 random sentences
 # Your code here
+
+def create_sentence(dict):
+
+    count = 0
+    sentence_count = 0
+    s = []
+    finished = False
+
+    while not finished:
+        if count == 0:
+            word = random.choice(list(dict))
+            print(word, count)
+            print(word[0].isupper())
+            print()
+
+            if word[0] == '"' or word[0].isupper():
+                s.append(word)
+                count += 1
+        if count > 0:
+            new_word = random.choice(list(dict[s[count-1]]))
+            print(new_word, count)
+
+            if new_word[-1] == '.' or new_word[-1] == '!' or new_word[-1] == '?' or (new_word[-1] == '"' and new_word[-2] == '.') or (new_word[-1] == '"' and new_word[-2] == '!') or (new_word[-1] == '"' and new_word[-2] == '?'):
+                s.append(new_word)
+                sentence_count += 1
+                if sentence_count == 4:
+                    finished = True
+                if sentence_count < 4:
+                    count = 0
+
+            else:
+                s.append(new_word)
+                count += 1
+
+    if finished:
+        return ' '.join(s)
+
+
+print(create_sentence(markov))
