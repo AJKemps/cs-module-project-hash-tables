0b1bc56ec992912e38cba5b0b669ed21b382a52e
Alex Kemper <alex.j.kemper@gmail.com>
2020-10-01 20:27:31 -0400

    all probs done

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -3,6 +3,9 @@ find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
 
+import itertools
+import random
+
 #q = set(range(1, 10))
 #q = set(range(1, 200))
 q = (1, 3, 4, 7, 12)
@@ -13,3 +16,25 @@ def f(x):
 
 # Your code here
 
+
+def create_perms(list1):
+
+    perms = list(itertools.product(list1, repeat=4))
+
+    return perms
+
+
+def sum_diff(perms):
+
+    success = []
+
+    for perm in perms:
+        print(perm[0])
+        if (f(perm[0]) + f(perm[1])) == (f(perm[2]) - f(perm[3])):
+            success.append(perm)
+
+    return success
+
+
+print(create_perms(q))
+print(sum_diff(create_perms(q)))
--- whiteboard_challenge.py
+++ whiteboard_challenge.py
@@ -22,15 +22,38 @@ print(count)
 print(min_nums)
 
 # Add up and print the sum of the all of the minimum elements of each inner array. Each array may contain additional arrays nested arbitrarily deep, in which case the minimum value for the nested array should be added to the total.
-# [
-#   [8, [4]],
-#   [[90, 91], -1, 3],
-#   [9, 62],
-#   [[-7, -1, [-56, [-6]]]],
-#   [201],
-#   [[76, 0], 18],
-# ]
+arr2 = [
+    [8, [4]],
+    [[90, 91], -1, 3],
+    [9, 62],
+    [[-7, -1, [-56, [-6]]]],
+    [201],
+    [[76, 0], 18],
+]
 # The expected output for the above input is:
 # 8 + 4 + 90 + -1 + 9 + -7 + -56 + -6 + 201 + 0 + 18 = 260
 # You may use whatever programming language you'd like.
 # Verbalize your thought process as much as possible before writing any code. Run through the UPER problem solving framework while going through your thought process.
+
+min_nums2 = []
+
+
+def get_min(arr):
+    for item in arr:
+        if isinstance(item, list):
+            get_min(item)
+
+        if isinstance(item, int):
+            min_num = item
+            for num in item:
+                if isinstance(num, list):
+                    get_min(num)
+                if isinstance(num, int):
+                    if num < min_num:
+                        min_num = num
+            min_nums2.append(min_num)
+
+    return min_nums
+
+
+print(get_min(arr2))
